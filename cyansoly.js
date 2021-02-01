ee863df1b18651226ae5e95a282975f66453f69f
Cyansoly <932258418@qq.com>
2021-02-02 00:41:37 +0800

    gsh

--- cyansoly.js
+++
@@ -1,9 +0,0 @@
-function initPage()  
-{  
-    var objLoading = document.getElementById("LoadingBar");  
-    if (objLoading != null)  
-    {  
-        objLoading.style.display = "none";  
-    }
-    window.onload = initPage()
-} 
